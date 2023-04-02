c76aac5a6c2064498fec4d05a870a681a0fa2dc8
Blamechance <vtommycao@gmail.com>
2023-04-02 16:43:57 +1000

    virtual environment

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 import os
 import time
+import sqlite3
 
-from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
 
-# Configure application - this let's flask know to use the "app.py" file
+# Configure application - this lets flask know to use the "app.py" file
 app = Flask(__name__)
-
