741ddecb3494fc53dca100835721e909ed8b3c43
Tommy Cao <vtommycao@gmail.com>
2023-04-07 11:16:53 +1000

    Added nav bar and layout skeleton for flask html content

--- app.py
+++ app.py
@@ -10,9 +10,39 @@ from werkzeug.security import check_password_hash, generate_password_hash
 # Configure application - this lets flask know to use the "app.py" file
 app = Flask(__name__)
 
+# Disable caching + enable debug mode:
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+app.debug = True
+
 @app.route('/', methods=["GET", "POST"])
 def index():
     return render_template("index.html") 
 
+@app.route('/team-dashboard', methods=["GET", "POST"])
+def teamDashboard():
+    return render_template("team-dashboard.html") 
+
+@app.route('/athletes', methods=["GET", "POST"])
+def athletes():
+    return render_template("athletes.html") 
+
+@app.route('/checkin', methods=["GET", "POST"])
+def checkin():
+    return render_template("checkin.html") 
+
+#Athelete pages - maybe they can share a single function to call? 
+@app.route('/tommy', methods=["GET", "POST"])
+def tommy():
+    return render_template("tommy.html") 
+
+@app.route('/nathan', methods=["GET", "POST"])
+def nathan():
+    return render_template("nathan.html") 
+
+@app.route('/raymond', methods=["GET", "POST"])
+def raymond():
+    return render_template("raymond.html") 
 
 
+if __name__ == "__main":
+    app.run(debug=True)
