d35cd4515a1bbb6abc7f5b7fcbe57cda69bac6fa
Tommy Cao <vtommycao@gmail.com>
2023-04-16 17:29:44 +1000

    def tommy() changes
    Added logic to create a list of last 12 months, based on current local month.
    To be used as x-axis for athlete chart when interacted with by event listener button.

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-import time
+import datetime
 import sqlite3
 
 from flask import Flask, flash, redirect, render_template, request, session
@@ -14,6 +14,13 @@ app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 app.debug = True
+
+#current time/date: 
+currentTimeDate = datetime.datetime.now()
+
+#return the current month as a digit
+currentMonth = int(currentTimeDate.strftime("%m"))
+
 
 @app.route('/', methods=["GET", "POST"])
 def index():
@@ -33,9 +40,33 @@ def checkin():
 
 #Athelete pages - maybe they can share a single function to call? 
 @app.route('/tommy', methods=["GET", "POST"])
-def tommy():
-    return render_template("tommy.html") 
+def tommy():    
+    tommy_12_month_x = [] #final list of months to pass to Tommy's chart
+    monthListDigits = [] #temp buffer to create list of months. 
 
+    #create a list of months in digit form: 
+    if currentMonth == 12:
+        monthListDigits	= ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
+    elif currentMonth < 12:
+        lastYrMonths = (12 - currentMonth)
+
+        #create a list counting backwards from current month
+        for i in range(currentMonth):
+            monthListDigits.insert(0, (currentMonth-i))
+            
+        #Adding months from last year: 
+        for j in range(12, (12- lastYrMonths), -1):
+            monthListDigits.insert(0, j)
+        
+        #Convert the list of digits into their month abbrev. forms
+        for k in range(12):
+            targetMonthText = datetime.date(1, int(monthListDigits[k]), 1).strftime('%B')
+            tommy_12_month_x.append(str(targetMonthText))
+            
+    print("Final month list to pass back for chart rendering: ", tommy_12_month_x)
+    return render_template("tommy.html", tommy_12_month_x = tommy_12_month_x) 
+        
 @app.route('/nathan', methods=["GET", "POST"])
 def nathan():
     return render_template("nathan.html") 
