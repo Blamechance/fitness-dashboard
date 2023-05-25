d6c3523567474791af3e8677753ce5123b765df1
Tommy Cao <vtommycao@gmail.com>
2023-05-26 09:50:09 +1000

    minor changes to variable naming fixed inconsistent snake casing + applied convention for constants

--- app.py
+++ app.py
@@ -21,28 +21,28 @@ app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 # Define path to upload folders
-app.config['weight_log_folder'] = 'files/weight_logs'
-app.config['training_log_folder'] = 'files/training_logs'
+app.config['WEIGHT_LOG_FOLDER'] = 'files/weight_logs'
+app.config['TRAINING_LOG_FOLDER'] = 'files/training_logs'
 app.config['MAX_CONTENT_PATH'] = 50000 #sample files average 20KB. 
 
 #Define appropriate CSV headers + byte length for validate function: 
-training_headers = "Date,Exercise,Category,Weight (kgs),Reps,Distance,Distance Unit,Time,Comment"
-weight_headers = "Date,Time,Measurement,Value,Unit,Comment"
-t_header_length = len(training_headers) #works on logic that chars are one byte
-w_header_length = len(weight_headers)
-valid_extensions = ('.csv', '.txt', '.CSV', '.TXT')
+TRAINING_HEADERS = "Date,Exercise,Category,Weight (kgs),Reps,Distance,Distance Unit,Time,Comment"
+WEIGHT_HEADERS = "Date,Time,Measurement,Value,Unit,Comment"
+T_HEADER_LENGTH = len(TRAINING_HEADERS) #works on logic that chars are one byte
+W_HEADER_LENGTH = len(WEIGHT_HEADERS)
+VALID_EXTENSIONS = ('.csv', '.txt', '.CSV', '.TXT')
 
 
-print("training header length: ", t_header_length)
-print("weight header length: ", w_header_length)
+print("training header length: ", T_HEADER_LENGTH)
+print("weight header length: ", W_HEADER_LENGTH)
 
 #current time/date: 
-currentTimeDate = datetime.datetime.now()
+CURRENT_TIME_DATE = datetime.datetime.now()
 
 #return the current month as a digit
-currentMonth = int(currentTimeDate.strftime("%m")) #%m prints month as digit
-currentYear = int(currentTimeDate.strftime("%Y")) #e.g 2013, 2019 etc.
-lastYear = currentYear - 1
+current_month = int(CURRENT_TIME_DATE.strftime("%m")) #%m prints month as digit
+CURRENT_YEAR = int(CURRENT_TIME_DATE.strftime("%Y")) #e.g 2013, 2019 etc.
+last_year = CURRENT_YEAR - 1
 
 
 @app.route('/', methods=["GET", "POST"])
@@ -88,118 +88,118 @@ def raymond():
     
 
 def fetch12mXAxis():
-    last12Months = [] #final list of months to pass to Tommy's chart
-    monthListDigits = [] #temp buffer to create list of months. 
+    last_12_months = [] #final list of months to pass to Tommy's chart
+    month_list_digits = [] #temp buffer to create list of months. 
 
     #create a list of months in digit form: 
-    if currentMonth == 12:
-        monthListDigits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    if current_month == 12:
+        month_list_digits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 
     
-    elif currentMonth < 12:
-        lastYrMonths = (12 - currentMonth)
+    elif current_month < 12:
+        last_yr_months = (12 - current_month)
 
         #create a list counting backwards from current month
-        for i in range(currentMonth):
-            monthListDigits.insert(0, (currentMonth - i))
+        for i in range(current_month):
+            month_list_digits.insert(0, (current_month - i))
             
         #Adding months from last year: 
-        for j in range(12, (12 - lastYrMonths), -1):
-            monthListDigits.insert(0, j)
+        for j in range(12, (12 - last_yr_months), -1):
+            month_list_digits.insert(0, j)
         
         #Convert the list of digits into their month abbrev. forms
-    for k in range(lastYrMonths):
-        targetMonthText = datetime.date(1, int(monthListDigits[k]), 1).strftime('%b')
-        last12Months.append(str(targetMonthText) + " " + str(lastYear))
+    for k in range(last_yr_months):
+        target_month_text = datetime.date(1, int(month_list_digits[k]), 1).strftime('%b')
+        last_12_months.append(str(target_month_text) + " " + str(last_year))
                 
-    for m in range(currentMonth):
-        targetMonthText = datetime.date(currentYear, int(monthListDigits[lastYrMonths+m]), 1).strftime('%b')
-        last12Months.append(str(targetMonthText) + " " + str(currentYear))
+    for m in range(current_month):
+        target_month_text = datetime.date(CURRENT_YEAR, int(month_list_digits[last_yr_months+m]), 1).strftime('%b')
+        last_12_months.append(str(target_month_text) + " " + str(CURRENT_YEAR))
             
-    return last12Months
+    return last_12_months
 
 def fetch6mXAxis():
-    last6Months = [] #final list of months to pass to Tommy's chart
-    monthListDigits = [] #temp buffer to create list of months.     
+    last_6_months = [] #final list of months to pass to Tommy's chart
+    month_list_digits = [] #temp buffer to create list of months.     
     
-    if currentMonth < 6:
-        lastYrMonths = (6 - currentMonth)
+    if current_month < 6:
+        last_yr_months = (6 - current_month)
 
     #create a list of months in digit form: 
-    if currentMonth >= 6:
+    if current_month >= 6:
         for i in range(7):
-            monthListDigits.insert(0, (currentMonth-i))
+            month_list_digits.insert(0, (current_month-i))
             
-    elif currentMonth < 6:
-        lastYrMonths = (6 - currentMonth)
+    elif current_month < 6:
+        last_yr_months = (6 - current_month)
 
         #create a list counting backwards from current month
-        for i in range(currentMonth):
-            monthListDigits.insert(0, (currentMonth-i))
+        for i in range(current_month):
+            month_list_digits.insert(0, (current_month-i))
                 
         #Adding months from last year:
-        for j in range(12, (12 - lastYrMonths), -1):
-            monthListDigits.insert(0, j)
+        for j in range(12, (12 - last_yr_months), -1):
+            month_list_digits.insert(0, j)
             
     #Convert the list of digits into their month abbrev. forms
     #prev months first: 
-    for k in range(lastYrMonths):
-        targetMonthText = datetime.date(lastYear, int(monthListDigits[k]), 1).strftime("%d %b, %Y")
-        last6Months.append(str(targetMonthText))
+    for k in range(last_yr_months):
+        target_month_text = datetime.date(last_year, int(month_list_digits[k]), 1).strftime("%d %b, %Y")
+        last_6_months.append(str(target_month_text))
 
-        targetMonthText = datetime.date(lastYear, int(monthListDigits[k]), 15).strftime("%d %b, %Y")
-        last6Months.append(str(targetMonthText))
+        target_month_text = datetime.date(last_year, int(month_list_digits[k]), 15).strftime("%d %b, %Y")
+        last_6_months.append(str(target_month_text))
 
                 
-    for m in range(lastYrMonths, 6, 1):
-        targetMonthText = datetime.date(1, int(monthListDigits[m]), 1).strftime('%b')
+    for m in range(last_yr_months, 6, 1):
+        target_month_text = datetime.date(1, int(month_list_digits[m]), 1).strftime('%b')
         
-        targetMonthText = datetime.date(currentYear, int(monthListDigits[m]), 1).strftime("%d %b, %Y")
-        last6Months.append(str(targetMonthText))
+        target_month_text = datetime.date(CURRENT_YEAR, int(month_list_digits[m]), 1).strftime("%d %b, %Y")
+        last_6_months.append(str(target_month_text))
 
-        targetMonthText = datetime.date(currentYear, int(monthListDigits[m]), 15).strftime("%d %b, %Y")
-        last6Months.append(str(targetMonthText))
+        target_month_text = datetime.date(CURRENT_YEAR, int(month_list_digits[m]), 15).strftime("%d %b, %Y")
+        last_6_months.append(str(target_month_text))
             
-    return last6Months
+    return last_6_months
     
 def fetch3mXAxis():
-    last3Months = [] #final list of months to pass to Tommy's chart
-    monthListDigits = [] #temp buffer to create list of months.     
+    last_3_months = [] #final list of months to pass to Tommy's chart
+    month_list_digits = [] #temp buffer to create list of months.     
     
-    if currentMonth < 3:
-        lastYrMonths = (3 - currentMonth)
+    if current_month < 3:
+        last_yr_months = (3 - current_month)
 
     #create a list of months in digit form: 
-    if currentMonth >= 3:
+    if current_month >= 3:
         for i in range(4):
-            monthListDigits.insert(0, (currentMonth-i))
+            month_list_digits.insert(0, (current_month-i))
             
-    elif currentMonth < 3:
-        lastYrMonths = (3 - currentMonth)
+    elif current_month < 3:
+        last_yr_months = (3 - current_month)
 
         #create a list counting backwards from current month
-        for i in range(currentMonth):
-            monthListDigits.insert(0, (currentMonth-i))
+        for i in range(current_month):
+            month_list_digits.insert(0, (current_month-i))
             
         #Adding months from last year: 
-        for j in range(12, (12 - lastYrMonths), -1):
-            monthListDigits.insert(0, j)
+        for j in range(12, (12 - last_yr_months), -1):
+            month_list_digits.insert(0, j)
         
     #Convert the list of digits into their month abbrev. forms
     for k in range(3):
         #code to change montListDigits to have the full correct date incl. year:
         #if month is Dec or Nov, it's from last year: 
-        if monthListDigits[k] >= 11: 
-            targetMonthText = datetime.date(1, int(monthListDigits[k]), 1).strftime('%b')
-            last3Months.append(str(targetMonthText) + " 01, " + str(lastYear))
-            last3Months.append(str(targetMonthText) + " 15, " + str(lastYear))
+        if month_list_digits[k] >= 11: 
+            target_month_text = datetime.date(1, int(month_list_digits[k]), 1).strftime('%b')
+            last_3_months.append(str(target_month_text) + " 01, " + str(last_year))
+            last_3_months.append(str(target_month_text) + " 15, " + str(last_year))
 
         else: 
-            targetMonthText = datetime.date(1, int(monthListDigits[k]), 1).strftime('%b')
-            last3Months.append(str(targetMonthText) + " 01, " + str(currentYear))
-            last3Months.append(str(targetMonthText) + " 15, " + str(currentYear))
+            target_month_text = datetime.date(1, int(month_list_digits[k]), 1).strftime('%b')
+            last_3_months.append(str(target_month_text) + " 01, " + str(CURRENT_YEAR))
+            last_3_months.append(str(target_month_text) + " 15, " + str(CURRENT_YEAR))
             
-    return last3Months
+    return last_3_months
 
 @app.route('/volume_analysis', methods=["POST"])
 def volume_analysis():
@@ -211,6 +211,10 @@ def volume_analysis():
     
 @app.route('/upload', methods=["POST"])
 def upload_file():
+    """
+    This function receives the file attached to the form submission for processing. 
+    If file is found to not be of .csv/.txt or data validation fails, then file is deleted and error code is returned. 
+    """
     print("Entered upload_file. ")
 
     #Received object with form data is a list-like structure (immutable dict): 
@@ -226,18 +230,17 @@ def upload_file():
         uploaded_file = request.files['userUpload']
         file_name = uploaded_file.filename
         
-        if not file_name.endswith(valid_extensions): # .csv, .CSV, .txt or .TXT
+        if not file_name.endswith(VALID_EXTENSIONS): # .csv, .CSV, .txt or .TXT
             print("File is not of .txt or .csv")
             return "File format error. Please export your file and try again. ", 400
         
         print("File name: ", file_name)
         #save to files folder -- extend this logic to ensure no duplicates: 
-        uploaded_file.save(os.path.join(app.config['training_log_folder'], file_name))
+        uploaded_file.save(os.path.join(app.config['TRAINING_LOG_FOLDER'], file_name))
         if validate_CSV(file_name, submission_type) == False:
             print("Bad upload - validation failed. Deleting file.")
-            os.remove((os.path.join(app.config['training_log_folder'], file_name)))
+            os.remove((os.path.join(app.config['TRAINING_LOG_FOLDER'], file_name)))
             return "File format error. Please export your file and try again. ", 400
-        
         return "Server file upload Success."        
     
     # Detect weight file and call processing function
@@ -247,17 +250,17 @@ def upload_file():
         uploaded_file = request.files['userUpload']
         file_name = uploaded_file.filename
         
-        if not file_name.endswith(valid_extensions): # .csv, .CSV, .txt or .TXT
+        if not file_name.endswith(VALID_EXTENSIONS): # .csv, .CSV, .txt or .TXT
             print("File is not of .txt or .csv")
             return "File format error. Please export your file and try again. ", 400
         
         print("File name: ", file_name)
         #save to files folder -- extend this logic to ensure no duplicates: 
-        uploaded_file.save(os.path.join(app.config['weight_log_folder'], file_name))
+        uploaded_file.save(os.path.join(app.config['WEIGHT_LOG_FOLDER'], file_name))
         validate_CSV(file_name, submission_type)
         
         if validate_CSV(file_name, submission_type) == False:
-            os.remove((os.path.join(app.config['weight_log_folder'], file_name)))
+            os.remove((os.path.join(app.config['WEIGHT_LOG_FOLDER'], file_name)))
             print("Bad upload - validation failed. Deleting file.")
             return "File format error. Please export your file and try again. ", 400
 
@@ -267,24 +270,26 @@ def upload_file():
 
 def validate_CSV(file_name, type):
     """
-    
+    This function checks that the uploaded file is:
+     - A text/CSV file
+     - of correct header format and length, for the respective chosen file type
     """
     print("Type is: ", type)
     
     if type == "training": 
         #set location of file: 
-        file_location = os.path.join(app.config['training_log_folder'], file_name)
+        file_location = os.path.join(app.config['TRAINING_LOG_FOLDER'], file_name)
         
         #open uploaded file
         reader = open(file_location, "r")
         reader.flush()
         
         #read all file contents: 
-        file_headers = reader.read(t_header_length)
+        file_headers = reader.read(T_HEADER_LENGTH)
         print(file_headers)
         
         #check if headers are as expected: 
-        if file_headers == training_headers:
+        if file_headers == TRAINING_HEADERS:
             print("File headers are valid - uploading file. ")
             reader.close
             return True
@@ -296,18 +301,18 @@ def validate_CSV(file_name, type):
     
     if type == "weight":
         #set location of file: 
-        file_location = os.path.join(app.config['weight_log_folder'], file_name)
+        file_location = os.path.join(app.config['WEIGHT_LOG_FOLDER'], file_name)
         
         #open uploaded file
         reader = open(file_location, "r")
         reader.flush()
         
         #read all file contents: 
-        file_headers = reader.read(w_header_length)
+        file_headers = reader.read(W_HEADER_LENGTH)
         print(file_headers)
         
         #check if headers are as expected: 
-        if file_headers == weight_headers:
+        if file_headers == WEIGHT_HEADERS:
             print("File headers are valid - uploading file.")
             reader.close
             return True
