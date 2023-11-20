0434e86087cdf5ad53ef98790290ff56d8ac2f53
Tommy Cao <vtommycao@gmail.com>
2023-11-20 14:49:07 +1100

    started playing with CSS

--- app.py
+++ app.py
@@ -32,6 +32,7 @@ app = Flask(__name__)
 # Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
+
 Session(app)
 
 # Registering blueprints
